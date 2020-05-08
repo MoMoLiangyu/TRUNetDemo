458e0712d57fd5e7c12d3dbc20cee3776b952f0c
mashiquan <mashiquan@86yingji.com>
2020-05-08 11:24:52 +0800

    init project

---
+++ TRUNetworking/Classes/LRUNetworking.h
@@ -0,0 +1,19 @@
+//
+//  LRUNetworking.h
+//  MADemo
+//
+//  Created by moliangyu on 2020/5/6.
+//  Copyright © 2020 LTWM. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LRUNetworking : NSObject
+
++(void)printTRUNetAction;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TRUNetworking/Classes/LRUNetworking.m
@@ -0,0 +1,18 @@
+//
+//  LRUNetworking.m
+//  MADemo
+//
+//  Created by moliangyu on 2020/5/6.
+//  Copyright © 2020 LTWM. All rights reserved.
+//
+
+#import "LRUNetworking.h"
+
+@implementation LRUNetworking
+
++(void)printTRUNetAction
+{
+    NSLog(@"打印: 打印printTRUNet");
+}
+
+@end
